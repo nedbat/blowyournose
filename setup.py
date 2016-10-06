7a88e758fa55fb5ca6b2e2fd04344d7e450d4d83
Ned Batchelder <ned@nedbatchelder.com>
2016-10-06 12:06:49 -0400

    Restructure as a package

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     author_email='ned@nedbatchelder.com',
     description='A nose plugin to clean out attributes on the test case',
     license='Apache 2.0',
-    py_modules=['blowyournose'],
+    packages=['blowyournose'],
     entry_points={
         'nose.plugins.0.10': [
-            'blowyournose = blowyournose:BlowYourNose'
+            'blowyournose = blowyournose.plugin:BlowYourNose'
         ]
     }
 )
