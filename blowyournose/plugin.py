561fb11acaf8ba0e58ff0fef4e3a2137c87271e1
Ned Batchelder <ned@nedbatchelder.com>
2016-10-06 14:02:06 -0400

    Clean out patches on methods

--- blowyournose/plugin.py
+++ blowyournose/plugin.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+
+import sys
+
 from nose.plugins import Plugin
 
 class BlowYourNose(Plugin):
@@ -8,12 +12,20 @@ class BlowYourNose(Plugin):
     # being run.
 
     def beforeTest(self, test):
+        # Record the pre-existing attributes.
         test.byn_attrs = set(dir(test.test))
 
     def afterTest(self, test):
         obj = test.test
+
+        # Delete any attribute that we didn't have at the beginning.
         for attr in dir(obj):
             if attr not in test.byn_attrs:
                 delattr(obj, attr)
+
+        # Is there a mock.patch on the test method?
+        method = getattr(obj, obj._testMethodName)
+        if hasattr(method, "patchings"):
+            method.im_func.patchings = None
 
         test.byn_cleaned = True
